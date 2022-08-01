e3782b2882ad9802ce7e3f8248989458560d1058
Samuel Lambert <fanmc34@gmail.com>
2022-08-01 13:50:53 -0400

    Updated PLA Calculator with more accurate calc

--- pla-damage-calc.js
+++ pla-damage-calc.js
@@ -1,5 +1,9 @@
 var elm = [0, 2, 3, 4, 7, 8, 9, 14, 15, 16, 25];
 
+function roundD(num) {
+    return Math.ceil(num - 0.5);
+}
+
 function doCalc() {
     // Attack Stuff
     var baseAttack = parseInt(document.getElementById("attack").value);
@@ -29,15 +33,15 @@ function doCalc() {
     // Stat Calc
     var elb = Math.round((Math.sqrt(baseAttack) * elm[el > 10 || el < 0 ? 10 : el] + level) / 2.5);
     var attack = Math.floor(Math.floor((level/50 + 1) * baseAttack / 1.5) * nature) + elb
+    attack *= document.getElementById("slow-start").checked ? 0.5 : 1.0;
+    attack *= document.getElementById("wild-might").checked ? 1.5 : 1.0;
 
     var defElb = Math.round((Math.sqrt(baseDefense) * elm[defEl > 10 || defEl < 0 ? 10 : defEl] + defLevel) / 2.5);
     var defense = Math.floor(Math.floor((defLevel/50 + 1) * baseDefense / 1.5) * defNature) + defElb
+    defense *= document.getElementById("def-wild-might").checked ? 1.5 : 1.0;
 
     var hpElb = Math.round((Math.sqrt(baseHP) * elm[hpEl > 10 || hpEl < 0 ? 10 : hpEl] + defLevel) / 2.5);
     var hp = Math.floor((defLevel/100 + 1) * baseHP + defLevel) + hpElb;
-
-    // Damage Calc
-    var baseMultiplier = (attack * 0.2 + level * 3 + 20) / (defense + 50);
 
     var eff = 1.0;
     eff *= document.getElementById("bad-eff-25").checked ? 0.4 : 1.0;
@@ -46,24 +50,74 @@ function doCalc() {
     eff *= document.getElementById("very-eff-2").checked ? 2.0 : 1.0;
     eff *= document.getElementById("very-eff-4").checked ? 2.5 : 1.0;
 
-    var stab = 1.0;
-    stab *= document.getElementById("stab").checked ? 1.25 : 1.0;
+    // DAMAGE CALC
+    // Initial
+    var initialAttSide = ((100 + attack) + (15 * level)) * basePower;
+    var initialDefSide = 50 + defense;
+    var initial = Math.floor(Math.floor(initialAttSide / initialDefSide) / 5);
 
-    var attackBoost = 1.0;
-    attackBoost *= document.getElementById("attack-boost").checked ? 1.5 : 1.0;
+    // a
+    var attackStage = 0;
+    if (document.getElementById("attack-boost-down").checked) attackStage = -1;
+    if (document.getElementById("attack-boost-neutral").checked) attackStage = 0;
+    if (document.getElementById("attack-boost-up").checked) attackStage = 1;
 
-    var crit = 1.0;
-    crit *= document.getElementById("crit").checked ? 1.5 : 1.0;
+    var defenseStage = 0;
+    if (document.getElementById("def-boost-down").checked) defenseStage = -1;
+    if (document.getElementById("def-boost-neutral").checked) defenseStage = 0;
+    if (document.getElementById("def-boost-up").checked) defenseStage = 1;
+    
+    var a = initial;
 
-    var defenseBoost = 1.0;
-    defenseBoost *= document.getElementById("def-boost").checked ? 2/3 : 1.0;
-
-    // Final Calc with ranges
-    var damageMax = baseMultiplier * basePower * eff * stab * attackBoost * crit * defenseBoost;
-    var ranges = [];
-    for (var i=0; i<16; i++)
+    if (attackStage != defenseStage)
     {
-        ranges[i] = Math.floor(damageMax * (i + 85) / 100);
+        var attackBoost = 1.0;
+        attackBoost *= document.getElementById("attack-boost-down").checked ? 0.66 : 1.0;
+        attackBoost *= document.getElementById("attack-boost-neutral").checked ? 1.0 : 1.0;
+        attackBoost *= document.getElementById("attack-boost-up").checked ? 1.5 : 1.0;
+
+        var defenseBoost = 1.0;
+        defenseBoost *= document.getElementById("def-boost-down").checked ? 1.5 : 1.0;
+        defenseBoost *= document.getElementById("def-boost-neutral").checked ? 1.0 : 1.0;
+        defenseBoost *= document.getElementById("def-boost-up").checked ? 0.66 : 1.0;
+
+        a = Math.floor((attackBoost * initial) * defenseBoost);
+    }
+
+    // b
+    var b = roundD(a * (document.getElementById("rain").checked ? 0.75 : 1.0));
+
+    // c
+    var c = Math.floor(b * (document.getElementById("crit").checked ? 1.5 : 1.0));
+
+    // d (Ranges)
+    var ranges = [];
+    for (var r=0; r<16; r++)
+    {
+        var d = Math.floor(c * ((r + 85) / 100));
+
+        // e
+        var e = roundD(d * (document.getElementById("stab").checked ? 1.25 : 1.0));
+
+        // f
+        var f = Math.floor(e * eff);
+
+        // g
+        var g = Math.floor(f * (document.getElementById("burn").checked ? 0.5 : 1.0));
+
+        // h
+        var h = Math.floor(g * (document.getElementById("drowsy").checked ? 1.33 : 1.0));
+
+        // i
+        var i = Math.floor(h * (document.getElementById("att-fixated").checked ? 1.5 : 1.0));
+
+        // j
+        var j = Math.floor(i * (document.getElementById("def-fixated").checked ? 1.33 : 1.0));
+
+        // k
+        var k = Math.floor(j * (document.getElementById("primed").checked ? 1.5 : 1.0));
+
+        ranges[r] = k;
     }
 
     var killingRanges = 0;
@@ -74,7 +128,7 @@ function doCalc() {
 
     // Change labels
     var atkVsDef = document.getElementById("atk-vs-def");
-    atkVsDef.textContent = "(" + attack + " Attack vs. " + defense + " Defense @ " + hp + " HP, before multipliers)";
+    atkVsDef.textContent = "(" + attack + " Attack vs. " + defense + " Defense @ " + hp + " HP, before most multipliers)";
 
     var textRanges = document.getElementById("ranges");
     textRanges.textContent = "[" + ranges.join(", ") + "]";
