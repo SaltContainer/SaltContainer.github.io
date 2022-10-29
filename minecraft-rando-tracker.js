1b08e6abe9706738d88d4b7e2be25e58e2527b62
Samuel Lambert <fanmc34@gmail.com>
2022-10-29 03:19:32 -0400

    MC Rando Tracker - Switched to jQuery Datatables

--- minecraft-rando-tracker.js
+++ minecraft-rando-tracker.js
@@ -10,7 +10,7 @@ class Loot {
     }
 }
 
-var tables = new Map();
+var tables = [];
 var selectedLoot = null;
 
 fetch('minecraft-rando-tracker/loot_tables.json')
@@ -24,54 +24,69 @@ fetch('minecraft-rando-tracker/loot_tables.json')
         console.log('Error while loading JSON: ' + err);
     });
 
+$(document).ready( function () {
+    $('#results').DataTable({
+        "data": tables,
+        "columns": [
+            { "data": "name", "defaultContent": "-----" },
+            { "data": "from.name", "defaultContent": "-----" },
+            { "data": "category", "defaultContent": "block" }
+        ],
+        "columnDefs": [
+            {
+                "render": function (data, type, row) {
+                    return "<img src=\"" + row.img + "\"> " + data;
+                },
+                "targets": 0
+            },
+            {
+                "visible": false,
+                "targets": 2
+            },
+            {
+                "render": function (data, type, row) {
+                    if (row.from == null) return "<img src=\"minecraft-rando-tracker/chest.png\"> -----";
+                    return "<img src=\"" + row.from.img + "\"> " + data;
+                },
+                "targets": 1
+            }
+        ],
+        initComplete: function () {
+            var column = this.api().column(0);
+            var categoryColumn = this.api().column(2);
+            var select = $('<select></select>')
+                .appendTo($(column.header()))
+                .on('change', function () {
+                    if ($(this).val() == "all") {
+                        categoryColumn.search('', true, false).draw();
+                    } else {
+                        var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                        categoryColumn.search(val ? '^' + val + '$' : '', true, false).draw();
+                    }
+                });
+
+            [["all","All"], ["block", "Blocks"], ["chest", "Chests"], ["entity", "Entities"], ["gameplay", "Gameplay"]].forEach(function (id, index) {
+                select.append('<option value="' + id[0] + '">' + id[1] + '</option>');
+            });
+        },
+    });
+} );
+
 function selectLoot() {
+    var selectedDiv = document.getElementById("selected-item");
+    var img = selectedDiv.getElementsByTagName("img")[0];
     if (selectedLoot == null) {
-        selectedLoot = tables[this.id];
+        selectedLoot = tables.get(this.id);
+        img.src = selectLoot.img;
+        img.alt = " " + selectLoot.name;
+        selectedDiv.style.display = "";
     } else {
-        var clickedLoot = tables[this.id];
+        var clickedLoot = tables.get(this.id);
         selectedLoot.drops = clickedLoot;
         clickedLoot.from = selectedLoot;
         updateResult(this, selectedLoot);
         selectedLoot = null;
-    }
-}
-
-function updateSearch() {
-    var input = document.getElementById("loot-search");
-    var filter = input.value.toUpperCase();
-    var table = document.getElementById("results-body");
-    var items = table.getElementsByTagName("tr");
-    var category = document.getElementById("category");
-
-    for (i = 0; i < items.length; i++) {
-        var loot = items[i].getElementsByTagName("td")[0];
-        var result = items[i].getElementsByTagName("td")[1];
-        var lootValue = loot.textContent || loot.innerText;
-        var resultValue = result.textContent || result.innerText;
-        var categoryValue = category.value;
-        var lootId = items[i].id;
-
-        if (matchesCategory(categoryValue, lootId) && (lootValue.toUpperCase().indexOf(filter) > -1 || resultValue.toUpperCase().indexOf(filter) > -1)) {
-            items[i].style.display = "";
-        } else {
-            items[i].style.display = "none";
-        }
-    }
-}
-
-function matchesCategory(category, lootId) {
-    var loot = tables.get(lootId);
-    switch (category) {
-        case "all":
-            return true;
-        case "blocks":
-            return loot.category == "block";
-        case "chests":
-            return loot.category == "chest";
-        case "entities":
-            return loot.category == "entity";
-        case "gameplay":
-            return loot.category == "gameplay";
+        selectedDiv.style.display = "none";
     }
 }
 
@@ -87,35 +102,6 @@ function initialData(data) {
     var resultsBody = document.getElementById("results-body");
     for (var i = 0; i < data.length; i++) {
         var loot = new Loot(data[i].id, data[i].name, data[i].img, data[i].category, data[i].contents);
-        tables.set(data[i].id, loot);
-
-        var tr = document.createElement("tr");
-        tr.id = data[i].id;
-        tr.addEventListener("click", selectLoot);
-        
-        var td1 = document.createElement("td");
-        var img1 = document.createElement("img");
-        img1.src = loot.img;
-        img1.alt = loot.name;
-        img1.className = "loot-icon";
-        td1.appendChild(img1);
-
-        var text1 = document.createTextNode(" " + loot.name);
-        td1.appendChild(text1);
-
-        var td2 = document.createElement("td");
-        var img2 = document.createElement("img");
-        img2.src = "minecraft-rando-tracker/chest.png"
-        img2.alt = "Unknown";
-        img2.className = "loot-icon";
-        td2.appendChild(img2);
-
-        var text2 = document.createTextNode(" -----");
-        td2.appendChild(text2);
-
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-    
-        resultsBody.appendChild(tr);
+        tables.push(loot);
     }
 }
